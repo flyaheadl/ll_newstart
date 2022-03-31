e95c9e6f09821ac02ea749d42bc411e59d149398
flyaheadl <64729207+flyaheadl@users.noreply.github.com>
2022-03-31 13:48:29 +0800

    Update liang_workspace.py

--- liang_workspace.py
+++ liang_workspace.py
@@ -13,7 +13,7 @@ from PIL import Image
 
 import numpy as np
 import pandas as pd
-from scipy.interpolate import Rbf  
+#from scipy.interpolate import Rbf  
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib as mpl
